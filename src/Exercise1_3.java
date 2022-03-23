6b244bae47f0759f5effe6144ef2d2373a984fc6
m475m <56003491+m475m@users.noreply.github.com>
2022-03-23 23:18:28 +0300

    Add exercise 1.3

---
+++ src/Exercise1_3.java
@@ -0,0 +1,7 @@
+public class Exercise1_3 {
+    public static void main(String[] args) {
+        String str = "744652652";
+        int number = Integer.parseInt(str);
+        System.out.println(number + 1);
+    }
+}
