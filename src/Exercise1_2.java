062b253c8049ef6520eb5738877b5d14773d1c91
m475m <56003491+m475m@users.noreply.github.com>
2022-03-23 22:53:10 +0300

    Add exercise 1.2

---
+++ src/Exercise1_2.java
@@ -0,0 +1,13 @@
+public class Exercise1_2 {
+public static void main(String[] args) {
+    System.out.print("byte: -128 to 127");
+    System.out.println("short: -32,768 to 32,767");
+    System.out.println("int: -2,147,483,648 to 2,147,483,647");
+    System.out.println("long: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807");
+    System.out.println("float: 1.40129846432481707e-45 to 3.40282346638528860e+38");
+    System.out.println("double: 4.94065645841246544e-324d to 1.79769313486231570e+308d");
+    System.out.println("boolean: true and false");
+    System.out.println("char: 0 to 65,535");
+}
+
+}
