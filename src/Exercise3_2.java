7983937d6c83c9cc94d42a205ccc40a13ac64b86
m475m <56003491+m475m@users.noreply.github.com>
2022-03-25 00:11:31 +0300

    Add exercise 3.2

---
+++ src/Exercise3_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercise3_2 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String str1 = "PHP упражнения и";
+        String str2 = "Упражнения на Java";
+        stringBuilder.append(str1);
+        stringBuilder.append(str2);
+        System.out.println(stringBuilder.toString());
+    }
+}
