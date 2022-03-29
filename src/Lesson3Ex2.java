d4452aa385d4b3a46bd83d51bbda16ebae8bdd91
m475m <56003491+m475m@users.noreply.github.com>
2022-03-30 00:07:33 +0300

    Change to cycle

--- src/Lesson3Ex2.java
+++ src/Lesson3Ex2.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lesson3Ex2 {
@@ -10,13 +9,26 @@ public class Lesson3Ex2 {
         arr[1] = scanner.nextInt();
         arr[2] = scanner.nextInt();
 
-        Arrays.parallelSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            int max = arr[i];
+            int max_i = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                    max_i = j;
+                }
+            }
+            if (i != max_i) {
+                int tmp = arr[i];
+                arr[i] = arr[max_i];
+                arr[max_i] = tmp;
+            }
+        }
 
-        System.out.print(arr[2]);
-        System.out.print(arr[1]);
         System.out.print(arr[0]);
+        System.out.print(arr[1]);
+        System.out.print(arr[2]);
 
     }
-
 
 }
