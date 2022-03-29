901ec74f35641e933e9d1efc796be40974f0f274
m475m <56003491+m475m@users.noreply.github.com>
2022-03-29 23:27:57 +0300

    Fix white spaces.

--- src/Lesson3Ex4.java
+++ src/Lesson3Ex4.java
@@ -2,7 +2,7 @@ public class Lesson3Ex4 {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j + " ");
+                System.out.print(j);
             }
             System.out.println();
             System.out.println();
